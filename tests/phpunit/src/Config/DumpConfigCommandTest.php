70ba11143d653a427a772d442018ca47370377a4
jm.gervais <jean-marie.gervais@dgfip.finances.gouv.fr>
2022-12-02 16:14:54 +0100

    test dumpConfig

---
+++ tests/phpunit/src/Config/DumpConfigCommandTest.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * This file is part of dgfip-si1/application.
+ *
+ */
+namespace DgfipSI1\ApplicationTests\Config;
+
+use DgfipSI1\Application\Config\DumpconfigCommand;
+use DgfipSI1\ConfigHelper\ConfigHelper;
+use DgfipSI1\testLogger\LogTestCase;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Output\NullOutput;
+
+/**
+ *
+ * @uses DgfipSI1\Application\Config\ConfiguredApplicationTrait::setConfig
+ * @uses DgfipSI1\Application\Config\ConfiguredApplicationTrait::getConfig
+ */
+class DumpConfigCommandTest extends LogTestCase
+{
+    /**
+     * @inheritDoc
+     *
+     */
+    public function setup(): void
+    {
+    }
+    /**
+     * test setInputOptions
+     *
+     * @covers \DgfipSI1\Application\Config\DumpConfigCommand
+     *
+     * @return void
+     */
+    public function testDumper()
+    {
+        $this->expectOutputString("schema:               []\n");
+        $cmd = new DumpconfigCommand();
+        $cmd->setConfig(new ConfigHelper());
+        $cmd->execute(new ArgvInput(), new NullOutput());
+    }
+}
