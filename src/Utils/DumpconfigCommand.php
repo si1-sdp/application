2f72872f2b504498599648b9aa728887d4c25127
jm.gervais <jean-marie.gervais@dgfip.finances.gouv.fr>
2022-12-13 17:05:53 +0100

    rename command

--- src/Utils/DumpconfigCommand.php
+++ src/Utils/DumpconfigCommand.php
@@ -12,7 +12,7 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-#[AsCommand(name: 'dumpconfig', description: 'Configuration dumper')]
+#[AsCommand(name: 'dump-config', description: 'Configuration dumper')]
 /**
  * Config dumper command
  */
